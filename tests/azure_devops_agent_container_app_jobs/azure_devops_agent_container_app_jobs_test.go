b80c4ddfdbc55153658c52ac76aea2a4248b813a
tjololo <1145298+tjololo@users.noreply.github.com>
2025-01-07 13:44:45 +0100

    first attemt to add terratests

---
+++ tests/azure_devops_agent_container_app_jobs/azure_devops_agent_container_app_jobs_test.go
@@ -0,0 +1,52 @@
+package azure_devops_agent_container_app_jobs
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/azure"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/utils/env"
+)
+
+func TestAzureDevopsAgentContainerAppJob(t *testing.T) {
+	t.Parallel()
+
+	subscriptionID := env.GetString("ALTINN_MODULES_TEST_ARM_SUBSCRIPTION_ID", "fail")
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: ".",
+		Vars: map[string]interface{}{
+			"subscription_id": subscriptionID,
+			"pat":             "NotARealPat",
+		},
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	verifyNonModuleResourcesExists(t, terraformOptions, subscriptionID)
+
+	verifyContainerAppResourcesExists(t, terraformOptions, subscriptionID)
+
+}
+
+func verifyNonModuleResourcesExists(t *testing.T, terraformOptions *terraform.Options, subscriptionID string) {
+	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	vnetName := terraform.Output(t, terraformOptions, "virtual_network_name")
+	subnetName := terraform.Output(t, terraformOptions, "subnet_name")
+	assert.True(t, azure.ResourceGroupExists(t, resourceGroupName, subscriptionID))
+	assert.True(t, azure.VirtualNetworkExists(t, vnetName, resourceGroupName, subscriptionID))
+	assert.True(t, azure.SubnetExists(t, subnetName, vnetName, resourceGroupName, subscriptionID))
+}
+
+func verifyContainerAppResourcesExists(t *testing.T, terraformOptions *terraform.Options, subscriptionID string) {
+	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	environmnetName := terraform.Output(t, terraformOptions, "environment_name")
+	placeholderJobName := terraform.Output(t, terraformOptions, "placeholder_job_name")
+	agentJobName := terraform.Output(t, terraformOptions, "agent_job_name")
+	assert.True(t, azure.ManagedEnvironmentExists(t, environmnetName, resourceGroupName, subscriptionID))
+	assert.True(t, azure.ContainerAppJobExists(t, placeholderJobName, resourceGroupName, subscriptionID))
+	assert.True(t, azure.ContainerAppJobExists(t, agentJobName, resourceGroupName, subscriptionID))
+}
---
+++ tests/azure_devops_agent_container_app_jobs/main.tf
@@ -0,0 +1,108 @@
+data "azurerm_client_config" "current" {}
+
+resource "random_string" "name" {
+  length  = 6
+  upper   = false
+  numeric = false
+  special = false
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = random_string.name.result
+  location = "Norway East"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${random_string.name.result}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "${random_string.name.result}-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/21"]
+  service_endpoints    = ["Microsoft.KeyVault"]
+}
+
+module "azure_devops_container-apps-agent" {
+  depends_on = [azurerm_resource_group.example]
+  source     = "../../modules/azure_devops_agent_container_app_jobs"
+
+  azp_org_url = "https://dev.azure.com/example"
+  azp_token   = base64encode(var.pat)
+
+  resource_prefix          = random_string.name.result
+  infrastructure_subnet_id = azurerm_subnet.example.id
+  resource_group_name      = azurerm_resource_group.example.name
+  kv_ip_rules = [
+    "8.8.8.8"
+  ]
+  additional_tags = {
+    key = "value"
+  }
+
+  agent_pool_name        = "example-azure-agent-pool"
+  agent_cpu              = "0.75"
+  agent_memory           = "1.5Gi"
+  agent_image            = "ghcr.io/altinn/altinn-platform/azure-devops-agent:v1.0.0"
+  agent_max_running_jobs = "18"
+}
+
+variable "pat" {
+  type        = string
+  sensitive   = true
+  description = "Azure Devops PAT"
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.14.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  features {}
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "virtual_network_name" {
+  value = azurerm_virtual_network.example.name
+}
+
+output "subnet_name" {
+  value = azurerm_subnet.example.name
+}
+
+output "environment_name" {
+  value = module.azure_devops_container-apps-agent.azurerm_container_app_environment_name
+}
+
+output "placeholder_job_name" {
+  value = module.azure_devops_container-apps-agent.azurerm_container_app_placeholder_job_name
+}
+
+output "agent_job_name" {
+  value = module.azure_devops_container-apps-agent.azurerm_container_app_agent_job_name
+}
+
+output "key_vault_name" {
+  value = module.azure_devops_container-apps-agent.azurerm_key_vault_name
+}
